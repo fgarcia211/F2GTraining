715f43a719ea06de3874f8c779103f9b2a4e8efc
fgarcia211 <fergarcia002100@outlook.es>
2023-03-03 00:10:32 +0100

    bbdd script + full user except token

--- F2GTraining/Controllers/UsuariosController.cs
+++ F2GTraining/Controllers/UsuariosController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using F2GTraining.Models;
+using F2GTraining.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace F2GTraining.Controllers
 {
     public class UsuariosController : Controller
     {
+        private RepositoryUsuarios repo;
+
+        public UsuariosController(RepositoryUsuarios repo)
+        {
+            this.repo = repo;
+        }
+
         public IActionResult InicioSesion()
         {
             return View();
@@ -12,7 +21,18 @@ namespace F2GTraining.Controllers
         [HttpPost]
         public IActionResult InicioSesion(string usuario, string contrasenha)
         {
-            return RedirectToAction("InicioSesion");
+            Usuario user = this.repo.GetUsuarioNamePass(usuario, contrasenha);
+            
+            if (user != null)
+            {
+                //guardar en sesion el usuario
+                return RedirectToAction("MenuEquipo","Equipos");
+            }
+            else
+            {
+                ViewData["ERROR"] = "ERROR: Credenciales de acceso erróneas";
+                return View();
+            }
         }
 
         public IActionResult RegistroUsuario()
@@ -21,9 +41,39 @@ namespace F2GTraining.Controllers
         }
 
         [HttpPost]
-        public IActionResult RegistroUsuario(string usuario, string contrasenha, string correo, string telefono)
+        public IActionResult RegistroUsuario(string usuario, string contrasenha, string correo, int telefono)
         {
-            return RedirectToAction("InicioSesion");
+            if (usuario.Count() > 16)
+            {
+                ViewData["ERROR"] = "ERROR: EL NOMBRE DE USUARIO NO PUEDE TENER MAS DE 16 CARACTERES";
+
+            }else if (this.repo.CheckUsuarioRegistro(usuario))
+            {
+                ViewData["ERROR"] = "ERROR: EL NOMBRE DE USUARIO INTRODUCIDO YA EXISTE";
+            }
+            else if (this.repo.CheckCorreoRegistro(correo))
+            {
+                ViewData["ERROR"] = "ERROR: EL CORREO ELECTRONICO INTRODUCIDO YA EXISTE";
+            }
+            else if (this.repo.CheckTelefonoRegistro(telefono))
+            {
+                ViewData["ERROR"] = "ERROR: EL TELEFONO INTRODUCIDO YA EXISTE";
+            }
+            else if (contrasenha.Count() <= 8)
+            {
+                ViewData["ERROR"] = "ERROR: LA CONTRASEÑA DEBE TENER UN MINIMO DE 8 CARACTERES";
+            }
+
+            if (ViewData["ERROR"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                this.repo.InsertUsuario(usuario, correo, contrasenha, telefono);
+                TempData["MENSAJE"] = "Usuario registrado correctamente";
+                return RedirectToAction("InicioSesion");
+            }
         }
 
         public IActionResult CerrarSesion()
--- F2GTraining/Repositories/RepositoryUsuarios.cs
+++ F2GTraining/Repositories/RepositoryUsuarios.cs
@@ -4,12 +4,54 @@ using F2GTraining.Data;
 using F2GTraining.Models;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System.Numerics;
 
 #region PROCEDURES
 
-/*CREATE PROCEDURE SP_INSERT_USUARIO (@NOMBRE NVARCHAR(50),@CORREO NVARCHAR(100), @CONTRASENIA NVARCHAR(50), @TELEFONO INT)
+/*CREATE OR ALTER PROCEDURE SP_INSERT_USUARIO (@NOMBRE NVARCHAR(50),@CORREO NVARCHAR(100), @CONTRASENIA NVARCHAR(50), @TELEFONO INT)
 AS
-    INSERT INTO USUARIOS VALUES ((SELECT MAX(ID) FROM USUARIOS)+1,@NOMBRE,@CORREO,@CONTRASENIA,@TELEFONO,NULL)
+	INSERT INTO USUARIOS VALUES ((SELECT MAX(ID) FROM USUARIOS)+1,@NOMBRE,@CORREO,@CONTRASENIA,@TELEFONO,NULL)
+GO
+
+CREATE OR ALTER PROCEDURE SP_FIND_USUARIO (@NOMBRE NVARCHAR(50), @CONTRASENIA NVARCHAR(50))
+AS
+	SELECT ID,NOM_USUARIO,CORREO,CONTRASENIA,TELEFONO,ISNULL(TOKEN,'SIN TOKEN') AS TOKEN FROM USUARIOS
+	WHERE NOM_USUARIO = @NOMBRE AND CONTRASENIA = @CONTRASENIA
+GO
+
+CREATE OR ALTER PROCEDURE SP_FIND_TOKEN (@TOKEN NVARCHAR(100))
+AS
+	SELECT ID,NOM_USUARIO,CORREO,CONTRASENIA,TELEFONO,ISNULL(TOKEN,'SIN TOKEN') AS TOKEN FROM USUARIOS
+	WHERE TOKEN = @TOKEN
+GO
+
+CREATE OR ALTER PROCEDURE SP_FIND_NOM_USUARIO (@NOMBRE NVARCHAR(50))
+AS
+	SELECT ID,NOM_USUARIO,CORREO,CONTRASENIA,TELEFONO,ISNULL(TOKEN,'SIN TOKEN') AS TOKEN FROM USUARIOS
+	WHERE NOM_USUARIO = @NOMBRE
+GO
+
+CREATE OR ALTER PROCEDURE SP_FIND_CORREO (@CORREO NVARCHAR(50))
+AS
+	SELECT ID,NOM_USUARIO,CORREO,CONTRASENIA,TELEFONO,ISNULL(TOKEN,'SIN TOKEN') AS TOKEN FROM USUARIOS
+	WHERE CORREO = @CORREO
+GO
+
+CREATE OR ALTER PROCEDURE SP_FIND_TELEFONO (@TELEFONO INT)
+AS
+	SELECT ID,NOM_USUARIO,CORREO,CONTRASENIA,TELEFONO,ISNULL(TOKEN,'SIN TOKEN') AS TOKEN FROM USUARIOS
+	WHERE TELEFONO = @TELEFONO
+GO
+
+CREATE OR ALTER PROCEDURE SP_FIND_TOKEN (@TOKEN NVARCHAR(100))
+AS
+	SELECT ID,NOM_USUARIO,CORREO,CONTRASENIA,TELEFONO,ISNULL(TOKEN,'SIN TOKEN') AS TOKEN FROM USUARIOS
+	WHERE TOKEN = @TOKEN
+GO
+
+CREATE OR ALTER PROCEDURE SP_UPDATE_TOKEN (@OLDTOKEN NVARCHAR(100), @NEWTOKEN NVARCHAR(100))
+AS
+	UPDATE USUARIOS SET TOKEN = @NEWTOKEN WHERE TOKEN = @OLDTOKEN
 GO*/
 
 #endregion
@@ -29,12 +71,99 @@ namespace F2GTraining.Repositories
             string sql = "SP_INSERT_USUARIO @NOMBRE, @CORREO, @CONTRASENIA, @TELEFONO";
 
             SqlParameter pamNom = new SqlParameter("@NOMBRE", nombre);
-            SqlParameter pamCor = new SqlParameter("@CORREO", correo);
+            SqlParameter pamCor = new SqlParameter("@CORREO", correo.ToLower());
             SqlParameter pamCon = new SqlParameter("@CONTRASENIA", contrasenia);
             SqlParameter pamTel = new SqlParameter("@TELEFONO", telefono);
 
             await this.context.Database.ExecuteSqlRawAsync(sql, pamNom, pamCor, pamCon, pamTel);
 
         }
+
+        public Usuario GetUsuarioNamePass(string nombre, string contrasenia)
+        {
+            string sql = "SP_FIND_USUARIO @NOMBRE, @CONTRASENIA";
+            SqlParameter pamNom = new SqlParameter("@NOMBRE", nombre);
+            SqlParameter pamCon = new SqlParameter("@CONTRASENIA", contrasenia);
+            var consulta = this.context.Usuarios.FromSqlRaw(sql, pamNom, pamCon);
+            Usuario user = consulta.AsEnumerable().FirstOrDefault();
+            return user;
+
+        }
+
+        public Usuario GetUsuarioToken(string token)
+        {
+            string sql = "SP_FIND_TOKEN @TOKEN";
+            SqlParameter pamTok = new SqlParameter("@TOKEN", token);
+            var consulta = this.context.Usuarios.FromSqlRaw(sql, pamTok);
+            Usuario user = consulta.AsEnumerable().FirstOrDefault();
+            return user;
+
+        }
+
+        public bool CheckTelefonoRegistro(int telefono)
+        {
+            string sql = "SP_FIND_TELEFONO @TELEFONO";
+            SqlParameter pamTel = new SqlParameter("@TELEFONO", telefono);
+            var consulta = this.context.Usuarios.FromSqlRaw(sql, pamTel);
+            Usuario user = consulta.AsEnumerable().FirstOrDefault();
+
+            if (user == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            
+        }
+
+        public bool CheckUsuarioRegistro(string nombre)
+        {
+            string sql = "SP_FIND_NOM_USUARIO @NOMBRE";
+            SqlParameter pamNom = new SqlParameter("@NOMBRE", nombre);
+            var consulta = this.context.Usuarios.FromSqlRaw(sql, pamNom);
+            Usuario user = consulta.AsEnumerable().FirstOrDefault();
+
+            if (user == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+        public bool CheckCorreoRegistro(string correo)
+        {
+            string sql = "SP_FIND_CORREO @CORREO";
+            SqlParameter pamCor = new SqlParameter("@CORREO", correo.ToLower());
+            var consulta = this.context.Usuarios.FromSqlRaw(sql, pamCor);
+            Usuario user = consulta.AsEnumerable().FirstOrDefault();
+
+            if (user == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+        public async Task UpdateToken(string oldtoken, string newtoken)
+        {
+            string sql = "SP_UPDATE_TOKEN @OLDTOKEN, @NEWTOKEN";
+
+            SqlParameter pamOld = new SqlParameter("@OLDTOKEN", oldtoken);
+            SqlParameter pamNew = new SqlParameter("@CORREO", newtoken);
+
+            await this.context.Database.ExecuteSqlRawAsync(sql, pamOld, pamNew);
+
+        }
+
     }
 }
