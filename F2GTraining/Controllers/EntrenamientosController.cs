f0726c9a05d4d03d1e438bb2a4688c8a9f4feee6
fgarcia211 <fergarcia002100@outlook.es>
2023-03-13 14:20:55 +0100

    listasesiones

---
+++ F2GTraining/Controllers/EntrenamientosController.cs
@@ -0,0 +1,22 @@
+﻿using F2GTraining.Extensions;
+using F2GTraining.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace F2GTraining.Controllers
+{
+    public class EntrenamientosController : Controller
+    {
+        public IActionResult ListaSesiones(int idequipo)
+        {
+            Usuario user = HttpContext.Session.GetObject<Usuario>("USUARIO");
+
+            if (user == null)
+            {
+                return RedirectToAction("InicioSesion", "Usuarios");
+            }
+
+            ViewData["MENSAJE"] = "Ver sesiones para el equipo con ID" + idequipo;
+            return View();
+        }
+    }
+}
