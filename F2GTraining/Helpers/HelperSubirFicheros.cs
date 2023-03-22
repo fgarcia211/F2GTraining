111153212f934da0be9b94c38d9d8a17a9fbdb52
fgarcia211 <fergarcia002100@outlook.es>
2023-03-22 10:23:48 +0100

    COMMIT PRELOGIN

--- F2GTraining/Helpers/HelperSubirFicheros.cs
+++ F2GTraining/Helpers/HelperSubirFicheros.cs
@@ -10,19 +10,13 @@
 
         public async Task<string> UploadFileAsync(IFormFile file, string nombre)
         {
-            string fileName = nombre + ".png";
+            string fileName = nombre.Replace(" ", string.Empty) + ".png";
             string path = this.helperRuta.MapPath(fileName);
 
             using (Stream stream = new FileStream(path, FileMode.Create))
             {
                 await file.CopyToAsync(stream);
             }
-
-            string[] separatedRoute = path.Split("\\");
-
-            string routeFileRoot = "~/" 
-                + separatedRoute[separatedRoute.Count() - 2] + "/" 
-                + separatedRoute[separatedRoute.Count() - 1];
 
             return fileName;
         }
